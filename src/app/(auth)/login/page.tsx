a584fdc627e4db1e33b8bdc495cbb1d95bb26ea5
Jerryton surya <jerry@swatle.ai>
2025-09-10 09:24:38 +0000

    remove signup page, I want to make this app as private so I will add the

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -49,12 +49,6 @@ export default function LoginPage() {
             Login with Google
           </Button>
         </div>
-        <div className="mt-4 text-center text-sm">
-          Don&apos;t have an account?{" "}
-          <Link href="/signup" className="underline">
-            Sign up
-          </Link>
-        </div>
       </CardContent>
     </Card>
   )
